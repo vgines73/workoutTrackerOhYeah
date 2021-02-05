8e67d364cdda5eb6e4147f4fc67703340ec0c371
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2021-02-05 14:13:02 -0800

    front end connected. able to see all pages. also see the data for exercises; type, name, weight, sets, reps, duration.

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
 // require dependencies
-
 const express = require("express");
 const mongoose = require("mongoose");
 
+// set up PORT
 const PORT = process.env.PORT || 3000;
 
+// set up express
 const app = express();
 
+// set up express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// static directory
 app.use(express.static("public"));
 
+
+// front end route
+const htmlRouter = require("./routes/html-routes")
+
+// invoke routes
+htmlRouter(app)
+
+// connect to database
 mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
-
 });
 
+// listening on port
 app.listen(PORT, () => {
     console.log(`App running on PORT http://localhost:${PORT}`);
 });
