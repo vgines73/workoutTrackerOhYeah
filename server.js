456625fd3bfe07eba94c8f4e2f5743581dfe544c
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2021-02-05 13:53:08 -0800

    created server; clicked on buttons and shows exercise, excercise id, and stats for api.

--- server.js
+++ server.js
@@ -1,4 +1,24 @@
+// require dependencies
 
+const express = require("express");
+const mongoose = require("mongoose");
 
+const PORT = process.env.PORT || 3000;
 
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on PORT http://localhost:${PORT}`);
+});
 
