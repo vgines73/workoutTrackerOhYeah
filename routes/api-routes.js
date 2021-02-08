d44af40804c427a402afafe42bb9d027faa0c233
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2021-02-07 21:15:26 -0800

    range issue solved. syntax issue.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,68 +4,67 @@ const Workout = require("../models/workout")
 module.exports = (app) => {
     // to find last workout 
     app.get("/api/workouts", (req, res) => {
-          // console.log(req.body)
+        // console.log(req.body)
         Workout.find()
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
+            .then(data => {
+                res.json(data);
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            });
     });
     // to add update an exercise by id
     app.put("/api/workouts/:id", (req, res) => {
         //console.log(req.body, req.params.id)
-        Workout.updateOne( 
+        Workout.updateOne(
             {
                 _id: req.params.id,
             },
             {
                 $set: {
-                    
+
                     exercises: req.body.exercises,
                     day: Date.now(),
-                   
+
                 }
             }
         )
-        .then(data => {
-            // console.log(data)
-            res.json(data);
-           
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
+            .then(data => {
+                // console.log(data)
+                res.json(data);
+
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            });
 
     });
     // to create a workout
     app.post("/api/workouts", (req, res) => {
         // console.log(req.body, req.params)
         Workout.create({})
-        .then(data => {
-           // console.log(data)
-           res.json(data);
-           res.send("hello from create post")
-           
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
+            .then(data => {
+                console.log("DATA: " + data)
+                res.json(data);
+                // res.send("hello from create post")
+
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            });
     });
-      
-    app.get("/api/workouts/range"), (req, res) => {
+
+    app.get("/api/workouts/range", (req, res) => {
         // console.log(res)
-        Workout.find({})
-        
-        .then(data => {
+        // res.send("HELLO");
+        Workout.find({}).then(data => {
             console.log(data)
             res.json(data);
         })
-        .catch(err => {
-            res.status(400).json(err);
-        }); 
-    }
+            .catch(err => {
+                res.status(400).json(err);
+            });
+    })
 
 };
 
